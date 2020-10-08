6318ca88659ce7a1d64f9f14672f737932163ca2
Irineu333 <mmmirineusilva@gmail.com>
2020-10-08 14:50:57 -0300

    Criei um adapter para gerenciar o RecyclerView

---
+++ app/src/main/java/com/neu/mygithub/adapter/RecyclerAdapter.kt
@@ -0,0 +1,71 @@
+package com.neu.mygithub.adapter
+
+import android.content.Context
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.neu.mygithub.R
+import com.neu.mygithub.github.model.Repo
+
+class RecyclerAdapter(
+    private val context: Context,
+    private var listRepos: MutableList<Repo> = mutableListOf(),
+    private val onClickListener: OnClickListener
+) : RecyclerView.Adapter<RecyclerAdapter.MyHolder>() {
+
+
+    class MyHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        private val nameTextView : TextView = itemView.findViewById(R.id.nameTextView)
+        private val loginTextView : TextView = itemView.findViewById(R.id.loginTextView)
+
+        fun atualiza(repoName : String, userLogin: String)
+        {
+            nameTextView.text = repoName
+            loginTextView.text = userLogin
+        }
+    }
+
+    fun setRepos(listRepos : List<Repo>, origem : String = "")
+    {
+        this.listRepos = listRepos.toMutableList()
+        Log.d("RecyclerAdapter", "setRepos $origem, count ${listRepos.size} == ${this.listRepos.size}")
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyHolder {
+        val view =
+            LayoutInflater.from(context).inflate(R.layout.item_recyclerview, parent, false)
+
+        return MyHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: MyHolder, position: Int) {
+
+        //objects
+        val repo = listRepos[position]
+        val author = repo.owner
+
+        //strings
+        val repoName = repo.name
+        val userLogin = author?.login ?: "nulo"
+
+        //configura MyHolder
+        holder.atualiza(repoName, userLogin)
+
+        holder.itemView.setOnClickListener {
+            onClickListener.onClick(repo)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return listRepos.size
+    }
+
+    interface OnClickListener {
+        fun onClick(data: Repo)
+    }
+}
