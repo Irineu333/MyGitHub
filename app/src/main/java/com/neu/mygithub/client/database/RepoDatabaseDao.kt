ee167322e453fe44b3135506f1e615de0fb489af
Irineu333 <mmmirineusilva@gmail.com>
2020-10-13 10:08:20 -0300

    Criação as funções update e getRepoAtId no Dao

--- app/src/main/java/com/neu/mygithub/client/database/RepoDatabaseDao.kt
+++ app/src/main/java/com/neu/mygithub/client/database/RepoDatabaseDao.kt
@@ -1,9 +1,6 @@
 package com.neu.mygithub.client.database
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
+import androidx.room.*
 import com.neu.mygithub.github.model.Repo
 
 @Dao
@@ -14,6 +11,12 @@ interface RepoDatabaseDao {
     @Query("SELECT * FROM repository_table")
     fun getRepoAll(): List<Repo>?
 
+    @Query("SELECT * from repository_table WHERE id = :id")
+    fun getRepoAtId(id : Int): Repo?
+
     @Query("SELECT * from author_table WHERE login = :login")
     fun getOwner(login : String): Repo.Owner?
+
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    fun update(repo : Repo)
 }
